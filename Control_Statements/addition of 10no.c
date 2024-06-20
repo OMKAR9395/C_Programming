b1269b0d632d3677fdf6903281ac24602519bb2a
omkar mohite <omkarmohite85@gmail.com>
2024-06-20 16:38:09 +0530

    without control statements

---
+++ Control_Statements/addition of 10no.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n1 = 0,n2 = 0,n3 = 0,n4 = 0,n5 = 0,sum = 0;
+    printf("\n enter a number 1= ");
+    scanf("%d",&n1);
+    printf("\n enter a number 2= ");
+    scanf("%d",&n2);
+    printf("\n enter a number 3= ");
+    scanf("%d",&n3);
+    printf("\n enter a number 4= ");
+    scanf("%d",&n4);
+    printf("\n enter a number 5= ");
+    scanf("%d",&n5);
+
+    sum = n1 + n2 + n3 + n4 + n5;
+
+    printf("\n addition of %d,%d,%d,%d,%d is = %d",n1,n2,n3,n4,n5,sum);
+    getch();
+    return 0;
+}
