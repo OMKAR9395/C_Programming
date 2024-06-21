2bbc9ad8d2ea64e4e57b02854d2010f6cef8e3ee
omkar mohite <omkarmohite85@gmail.com>
2024-06-21 20:35:25 +0530

    for

---
+++ loop/basic counter for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    for (int i = 1; i<=5; i++)
+    {
+        printf("\n counter: %d,i");
+    }
+    printf("\n thanks!");
+    getch();
+    return 0;
+}
