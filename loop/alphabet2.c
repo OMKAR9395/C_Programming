b22e4277a92b0a24f39ccaac52c71943a331c64a
omkar mohite <omkarmohite85@gmail.com>
2024-06-21 20:29:53 +0530

    while

---
+++ loop/alphabet2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    char spt='\0',ept='\0';
+    printf("\n enter a start point= ");
+    scanf("%c",&spt);
+    fflush(stdin);
+    printf("\n enter a end point: ");
+    scanf("%c",&ept);
+    while (spt<=ept)
+    {
+        printf("\n %c",spt);
+        spt++;
+    }
+    printf("\n thanks!!");
+    getch();
+    return 0;
+}
+
