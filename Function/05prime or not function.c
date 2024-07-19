86f487049b70eb5473576927f04d64d20e47ed50
omkar mohite <omkarmohite85@gmail.com>
2024-07-19 21:33:50 +0530

    prime

---
+++ Function/05prime or not function.c
@@ -0,0 +1,39 @@
+///with out using function
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No =0,i=0;
+    printf("\n Enter positive integer number : ");
+    scanf("%d",&No);
+
+    if(No < 0)
+    {
+        printf("\n Invalid input!!");
+    }
+    else if(No==0||No==1)
+    {
+        printf("\n Number Is Neutral");
+    }
+    else
+    {
+        for(i=2;i<No;i++)
+        {
+            if(No%i==0)
+            {
+                break;
+            }
+        }
+        if(i==No)
+        {
+            printf("\n Given Number %d is Prime",No);
+        }
+        else
+        {
+            printf("\n given number %d is not prime",No);
+        }
+    }
+    printf("\n Thanks!!");
+    getch();
+    return 0;
+}
---
+++ Function/07prime or not function.c
@@ -0,0 +1,56 @@
+///using function print in main
+#include<stdio.h>
+#include<conio.h>
+int Prime(int);
+int main()
+{
+    int Num = 0,Ans=0;
+    printf("\n Enter Positive Integer Number : ");
+    scanf("%d",&Num);
+     if(Num<0)
+   {
+       printf("\n Invalid Input!!");
+   }
+   else if(Num==0||Num==1)
+   {
+       printf("\n Number is Neutral");
+   }
+   else
+    {
+        Ans = Prime(Num);
+        if (Ans == 1)
+        {
+           printf("\n Given number %d is Prime",Num);
+        }
+        else
+        {
+            printf("\n Given number %s is not Prime",Num);
+        }
+    }
+
+    printf("\n Thanks");
+    getch();
+    return 0;
+}
+int Prime(int No)
+{
+   int i=0;
+
+       for(i=2;i<No;i++)
+       {
+           if(No%i==0)
+
+           {
+               break;
+           }
+       }
+       if(i==No)
+       {
+           return 1;
+       }
+       else
+       {
+          return 0;
+       }
+    return;
+}
