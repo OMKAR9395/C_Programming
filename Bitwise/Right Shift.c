d4286a25665be7b14d62eef3090474a362f22efc
omkar mohite <omkarmohite85@gmail.com>
2024-08-29 16:17:22 +0530

    Right

---
+++ Bitwise/Right Shift.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No=0,Res=0;
+    printf("\n Enter A Number :");
+    scanf("%d",&No);
+    Res = No << 4;
+    printf("\n Result Right Shift By 3>> %d",Res);
+    getch();
+    return 0;
+}
