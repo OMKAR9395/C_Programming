2163e35077f93ff4cd61849e7706935187dba387
omkar mohite <omkarmohite85@gmail.com>
2024-08-31 13:05:26 +0530

    static

---
+++ Static/03 Global static.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+int Book_Read(int);
+static int No = 10;
+int main()
+{
+    int No = 0,Res = 0;
+    printf("\n You Read First Day Pages Count: ");
+    scanf("%d",&No);
+
+    Res = Book_Read(No);
+
+    printf("\n You Readed Total Pages = %d",Res);
+    getch();
+
+    printf("\n You Read Second Day Pages Count: ");
+    scanf("%d",&No);
+    Res = Book_Read(No);
+
+    printf("\n You Readed Total Pages = %d",Res);
+    getch();
+
+    printf("\n You Read Third Day Pages Count: ");
+    scanf("%d",&No);
+    Res = Book_Read(No);
+
+    printf("\n You Readed Total Pages = %d",Res);
+
+    getch();
+    return 0;
+}
+int Book_Read(int Num)
+{
+    No++;
+    No++;
+    No = No + Num;
+    return No;
+}
+
