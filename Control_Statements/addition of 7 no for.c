911e324885b6bc2294f15fa1f88470aaed6c97aa
OMKAR9395 <omkarmohite85@gmail.com>
2024-09-16 15:49:57 +0530

    Change

--- Control_Statements/addition of 7 no for.c
+++ Control_Statements/addition of 7 no for.c
@@ -5,11 +5,11 @@ int main()
     int no=0,sum=0,i=0;
     for(i=1;i<=7;i++)
     {
-        printf("\n enter  a number %d= ",i);
+        printf("\n Enter  a Number %d= ",i);
         scanf("%d",&no);
         sum=sum+no;             ///sum+=no
     }
-    printf("\n addition of given numbers =%d",sum);
+    printf("\n Addition of Given Numbers =%d",sum);
     getch();
     return 0;
 }
