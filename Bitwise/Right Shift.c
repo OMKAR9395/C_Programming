c8d78ef383eef860a70a07f548d91c5ee3a501eb
OMKAR9395 <omkarmohite85@gmail.com>
2024-09-21 17:14:02 +0530

    Assignment

--- Bitwise/Right Shift.c
+++ Bitwise/Right Shift.c
@@ -5,8 +5,8 @@ int main()
     int No=0,Res=0;
     printf("\n Enter A Number :");
     scanf("%d",&No);
-    Res = No << 4;
-    printf("\n Result Right Shift By 3>> %d",Res);
+    Res = No >> 2;
+    printf("\n Result Right Shift By 4>> %d",Res);
     getch();
     return 0;
 }
