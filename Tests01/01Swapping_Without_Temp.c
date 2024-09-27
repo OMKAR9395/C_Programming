13066b990cf08c4b1694d287ff4e77d0ac09ec19
OMKAR9395 <omkarmohite85@gmail.com>
2024-09-27 20:15:54 +0530

    Test

---
+++ Tests01/01Swapping_Without_Temp.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No1 = 0, No2 = 0;
+
+    printf("\n Enter First Number :");
+    scanf("%d",&No1);
+
+    printf("\n Enter Second Number :");
+    scanf("%d",&No2);
+
+    No1 = No1 + No2;
+    No2 = No1 - No2;
+    No1 = No1 - No2;
+
+    printf("\n After Swapping Number First is = %d & Second Number is = %d",No1,No2);
+
+    getch();
+    return 0;
+
+}
