c179d7615dbfe960cb4a3c5c358edc5bc8a19ed4
OMKAR9395 <omkarmohite85@gmail.com>
2024-09-27 20:30:17 +0530

    Test

---
+++ Tests01/02Factorial.c
@@ -0,0 +1,26 @@
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+//  Writw a program to find of factorial of given number .              //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Fact=0, Temp=0, Num=0;
+
+    printf("\n Enter a Number :");
+    scanf("%d",&Fact);
+
+    Temp = Fact;
+    Num = 1;
+    while(Temp > 0)
+    {
+        Num = Num * Temp;
+        Temp--;
+    }
+    printf("\n %d of Factorial is = %d",Fact,Num);
+
+    getch();
+    return 0;
+}
