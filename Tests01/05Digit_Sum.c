f04a3f15ad09cb1b3dbb62f2f12418be5f92b44c
OMKAR9395 <omkarmohite85@gmail.com>
2024-09-28 21:06:01 +0530

    Test

---
+++ Tests01/05Digit_Sum.c
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////////////////
+//                                                                  //
+//  Write a program to calculate Digit Sum of Given Number.         //
+//                                                                  //
+//////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<conio.h>
+
+int Digit_Sum(int);
+int main()
+{
+    int Num=0,Ret=0;
+
+    printf("\n Enter a  Number :");
+    scanf("%d",&Num);
+
+    Ret = Digit_Sum(Num);
+
+    printf("\n Digit Sum Of %d is = %d",Num,Ret);
+
+    getch();
+    return 0;
+}
+int Digit_Sum(int No)
+{
+    int Digit=0,Sum = 0;
+    while(No > 0)
+    {
+        Digit = No % 10 
+        Sum = Sum + Digit;
+        No = No / 10;
+        
+    }
+    return Sum;
+}
