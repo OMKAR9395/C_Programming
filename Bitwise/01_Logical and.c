3ccf75f06ef658b1d41d5b30477eb1a2477047da
OMKAR9395 <omkarmohite85@gmail.com>
2024-09-28 09:59:37 +0530

    Assignment

---
+++ Bitwise/01_Logical and.c
@@ -0,0 +1,14 @@
+///Bitwisw Logical And
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a = 12,b = 10,res=0;
+
+    res = a & b;
+    printf("a & b = %d\n", res); ///Answer is = 8
+
+    getch();
+    return 0;
+}
+
