130c15fd559b25a9b972b14ba2039d0214b5f871
OMKAR9395 <omkarmohite85@gmail.com>
2025-03-12 21:41:56 +0530

    Case Study

--- Case Study/07Student_Info.c
+++ Case Study/07Student_Info.c
@@ -8,9 +8,6 @@
 #include<stdlib.h>
 #include<string.h>
 
-void Accept_Student_Details();
-void Display_Student_Details();
-
 struct Stud
 {
     int Roll_No;
@@ -21,6 +18,9 @@ struct Stud
     float Total;
     float Per;
 };
+void Accept_Student_Details(struct Stud* sPtr,int Cnt);
+void Display_Student_Details(struct Stud* sPtr,int Cnt);
+
 int main()
 {
     int Cnt = 0;
@@ -31,64 +31,70 @@ int main()
 
     Stdptr = (struct Stud*) malloc (Cnt * sizeof(struct Stud));
 
-    for(int i=0; i<Cnt; i++)
+    if (Stdptr == NULL)
     {
-        printf("\n Enter Student %d Details => \n",i+1);
-        Accept_Student_Details(&Stdptr[i]);
+        printf("Memory allocation failed!\n");
+        return -1;
     }
+
+    Accept_Student_Details(Stdptr,Cnt);
+
 
     printf("\n Enter Any Key To Go Ahead =>");
     getch();
 
     printf("==========================You Entered Student Details Are Here =============================\n");
 
-    for(int i=0;i<Cnt;i++)
-    {
-        printf("\n %d Student Details => \n",i+1);
-        Display_Student_Details(&Stdptr[i]);
-    }
+    Display_Student_Details(Stdptr,Cnt);
+
     printf("\n Thanks For Using Our Application !!");
     getch();
     return 0;
 
 }
-void Accept_Student_Details(struct Stud *sPtr)
+void Accept_Student_Details(struct Stud* sPtr,int Cnt)
 {
-       printf("\n Enter Student Roll No : ");
-            scanf("%d", &sPtr->Roll_No);
+    for(int i=0; i<Cnt; i++)
+    {
+            printf("\n Enter Student Details :%d",i+101);
+            sPtr[i].Roll_No = i+101;
 
             fflush(stdin);
 
             printf("\n Enter Student Name : ");
-            gets(sPtr->Name);
+            gets(sPtr[i].Name);
 
             printf("\n Enter Physics Marks : ");
-            scanf("%d", &sPtr->Phy);
+            scanf("%d", &sPtr[i].Phy);
 
             printf("\n Enter Chemistry Marks : ");
-            scanf("%d", &sPtr->Chem);
+            scanf("%d", &sPtr[i].Chem);
 
             printf("\n Enter Mathematics Marks : ");
-            scanf("%d", &sPtr->Maths);
+            scanf("%d", &sPtr[i].Maths);
 
-            sPtr->Total = sPtr->Phy + sPtr->Chem + sPtr->Maths;
+            sPtr[i].Total = sPtr[i].Phy + sPtr[i].Chem + sPtr[i].Maths;
 
-            sPtr->Per = (sPtr->Total / 300) * 100;
+            sPtr[i].Per = (sPtr[i].Total / 3);
             getch();
+    }
             return;
 }
-void Display_Student_Details(struct Stud *sPtr)
+void Display_Student_Details(struct Stud* sPtr,int Cnt)
 {
-            printf("\n\t\t Roll Number       = %d", sPtr->Roll_No);
-            printf("\n\t\t Student Name      = %s", sPtr->Name);
-            printf("\n\t\t Physics Marks     = %d", sPtr->Phy);
-            printf("\n\t\t Chemistry Marks   = %d", sPtr->Chem);
-            printf("\n\t\t Mathematics Marks = %d", sPtr->Maths);
-            printf("\n\t\t Total Marks       = %0.0f", sPtr->Total);
-            printf("\n\t\t Percentage        = %0.3f", sPtr->Per);
+    for(int i=0;i<Cnt;i++)
+    {
+            printf("\n\t\t Roll Number       = %d", sPtr[i].Roll_No);
+            printf("\n\t\t Student Name      = %s", sPtr[i].Name);
+            printf("\n\t\t Physics Marks     = %d", sPtr[i].Phy);
+            printf("\n\t\t Chemistry Marks   = %d", sPtr[i].Chem);
+            printf("\n\t\t Mathematics Marks = %d", sPtr[i].Maths);
+            printf("\n\t\t Total Marks       = %0.0f", sPtr[i].Total);
+            printf("\n\t\t Percentage        = %0.3f", sPtr[i].Per);
 
             printf("\n\n =====================Thank You======================= ");
 
             getch();
+    }
             return;
 }
