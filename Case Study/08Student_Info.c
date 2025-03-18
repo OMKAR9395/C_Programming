c384e71469d918536e8b3ae2fac6092a8cfeed1e
OMKAR9395 <omkarmohite85@gmail.com>
2025-03-18 17:29:15 +0530

    case study

--- Case Study/08Student_Info.c
+++ Case Study/08Student_Info.c
@@ -20,37 +20,91 @@ struct Stud
 };
 void Accept_Student_Details(struct Stud* sPtr,int Cnt);
 void Display_Student_Details(struct Stud* sPtr,int Cnt);
-void How_Many_Student(int* Cnt, struct Stud** sPtr);
+void Search_Student_Details(struct Stud* sPtr, int);
+int Cnt = 0;
 int main()
 {
-    int* Cnt = 0;
+    int Choice=0,RNo =0;
+    char ch ='\0';
     struct Stud *Stdptr;
 
-    How_Many_Student(Cnt,Stdptr);
-
-    Accept_Student_Details(Stdptr,Cnt);
-
-
-    Display_Student_Details(Stdptr,Cnt);
-
-    printf("\n Thanks For Using Our Application !!");
-    getch();
-    return 0;
-
-}
-void How_Many_Student(int* Cnt, struct Stud** sPtr)
-{
     printf("\n Enter Count Of Student :");
     scanf("%d",&Cnt);
 
-    *sPtr = (struct Stud*) malloc (*Cnt * sizeof(struct Stud));
+    Stdptr = (struct Stud*) malloc (Cnt * sizeof(struct Stud));
 
-    if (sPtr == NULL)
+    if (Stdptr == NULL)
     {
         printf("Memory allocation failed!\n");
         return -1;
     }
+    up:
+    printf("\n****************************Enter Choice Number.*********************************");
+    printf("\n1.Acccept Student Details.\n2.Display Student Details.\n3.Search Student Details.\n4.Exit.");
+    scanf("%d",&Choice);
 
+    switch(Choice)
+    {
+    case 1:
+
+        Accept_Student_Details(Stdptr,Cnt);
+        system("cls");
+
+        goto up;
+        break;
+
+    case 2:
+
+        Display_Student_Details(Stdptr,Cnt);
+        system("cls");
+
+        goto up;
+        break;
+
+    case 3:
+        printf("\n Enter Student Roll No To Search = ");
+        scanf("%d", &RNo);
+
+        Search_Student_Details(Stdptr, RNo);
+        system("cls");
+
+        goto up;
+        break;
+    case 4:
+
+        printf("\n Are You Sure?? \n Do You Want To Exit??? (YES / NO) : ");
+
+        fflush(stdin);
+
+        ch = getchar();
+
+        if(ch == 'y' || ch == 'Y')
+        {
+            goto quit;
+        }
+
+        fflush(stdin);
+        system("cls");
+        goto up;
+        break;
+
+        default :
+
+        printf("\n\n Invalid Input Please Select Valid Choice.");
+        printf("\n Press Any Key To Continue ");
+
+        getch();
+        system("cls");
+
+        goto up;
+        break;
+
+    }
+    quit:
+
+    printf("\n Thanks For Using Our Application !!");
+    getch();
+    return 0;
 
 }
 void Accept_Student_Details(struct Stud* sPtr,int Cnt)
@@ -83,7 +137,8 @@ void Accept_Student_Details(struct Stud* sPtr,int Cnt)
 }
 void Display_Student_Details(struct Stud* sPtr,int Cnt)
 {
-     printf("==========================You Entered Student Details Are Here =============================\n");
+    printf("==========================You Entered Student Details Are Here =============================\n");
+
     for(int i=0;i<Cnt;i++)
     {
             printf("\n\t\t Roll Number       = %d", sPtr[i].Roll_No);
@@ -99,4 +154,38 @@ void Display_Student_Details(struct Stud* sPtr,int Cnt)
             getch();
     }
             return;
+}
+void Search_Student_Details(struct Stud* sPtr, int RNo)
+{
+
+     int i = 0;
+
+        for(i = 0; i < Cnt; i++)
+        {
+            if(sPtr[i].Roll_No == RNo)
+            {
+                printf("\n ==================*****======================= ");
+
+                printf("\n\n Searched Student Details =>\n ", i+1);
+                printf("\n\t\t Roll Number       = %d", sPtr[i].Roll_No);
+                printf("\n\t\t Student Name      = %s", sPtr[i].Name);
+                printf("\n\t\t Physics Marks     = %d", sPtr[i].Phy);
+                printf("\n\t\t Chemistry Marks   = %d", sPtr[i].Chem);
+                printf("\n\t\t Mathematics Marks = %d", sPtr[i].Maths);
+                printf("\n\t\t Total Marks       = %0.0f", sPtr[i].Total);
+                printf("\n\t\t Percentage        = %0.3f", sPtr[i].Per);
+
+                printf("\n\n ==================*****======================= ");
+
+                break;
+            }
+        }
+
+        if(i == Cnt)
+        {
+            printf("\n No Such Student Found!!!");
+        }
+
+        _getch();
+        return;
 }
