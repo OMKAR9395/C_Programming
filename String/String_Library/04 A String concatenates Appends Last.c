09ee29e8e7c51826b46b6cf4f1c39a6135989169
OMKAR9395 <omkarmohite85@gmail.com>
2025-04-22 22:15:37 +0530

    String Library Redimade Functions

---
+++ String/String_Library/04 A String concatenates Appends Last.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+
+int main()
+{
+    char str1[100] = {};
+    char str2[50] = {};
+
+    puts("\n Enter First String :");
+    gets(str1);
+
+    puts("\n Enter Second String To Append :");
+    gets(str2);
+
+    printf("\n Before Concatenation => str1 = %s, str2 = %s", str1, str2);
+
+    strcat(str1, str2);
+
+    printf("\n After Concatenation  => str1 = %s", str1);
+
+    getch();
+    return 0;
+}
+
