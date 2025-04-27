b434a95017fe2f7035664d7bee505b75df1cee74
OMKAR9395 <omkarmohite85@gmail.com>
2025-04-27 23:39:37 +0530

    String

--- String/String_Library/06 String Compare Two Strings.c
+++ String/String_Library/06 String Compare Two Strings.c
@@ -3,7 +3,7 @@
 #include <conio.h>
 int main()
 {
-    
+
     char FirstString[50] = "";
     char SecondString[50] = "";
     int ComparisonResult = 0;
@@ -30,7 +30,6 @@ int main()
     {
     printf("%s is less than %s", FirstString, SecondString);
     }
-
 
     getch();
     return 0;
