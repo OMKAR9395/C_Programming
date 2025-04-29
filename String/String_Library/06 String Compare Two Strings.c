d1e4d98b456e608395dac5739ee407a50402440d
OMKAR9395 <omkarmohite85@gmail.com>
2025-04-29 22:43:03 +0530

    Assignment

--- String/String_Library/06 String Compare Two Strings.c
+++ String/String_Library/06 String Compare Two Strings.c
@@ -15,7 +15,6 @@ int main()
     puts("\n Enter Second String :");
     gets(SecondString);
 
-
     ComparisonResult = strcmp(FirstString, SecondString);
 
     printf("\n Comparing %s and %s\n", FirstString, SecondString);
